d332ffff3b37a810159df3f938fc82f95d1bf32c
Christopher Carson <ChrisCarson81@gmail.com>
2023-11-03 13:32:02 -0700

    switching to local run

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 
 exports.runScript = async (req, res) => {
     // Define the command to run
-    const command = `npx cross-env PLAYWRIGHT_BROWSERS_PATH=0 SCRIPT=${req.body.script} MY_PASS=${req.body.password} npx playwright test ./tests --reporter=list`;
+    const command = `npx cross-env PLAYWRIGHT_BROWSERS_PATH=0 SCRIPT=salesreps MY_PASS=cxb*hna!jur4ntf9ZER npx playwright test ./tests --reporter=list`;
   
     // Execute the command
     exec(command, (error, stdout, stderr) => {
@@ -17,6 +17,26 @@ exports.runScript = async (req, res) => {
       res.status(200).send({ stdout, stderr });
     });
   };
+
+  exports.runScript()
+
+// exports.runScript = async (req, res) => {
+//     // Define the command to run
+//     const command = `npx cross-env PLAYWRIGHT_BROWSERS_PATH=0 SCRIPT=${req.body.script} MY_PASS=${req.body.password} npx playwright test ./tests --reporter=list`;
+  
+//     // Execute the command
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         // Send a response with the error
+//         // console.log(`exec error: ${error} ${stdout} ${stderr}`)
+//         res.status(500).send({ error: `exec error: ${error} ${stdout} ${stderr}` });
+//         return;
+//       }
+  
+//       // Send a response with the command's output
+//       res.status(200).send({ stdout, stderr });
+//     });
+//   };
 
 //   const req = {
 //     body: {
